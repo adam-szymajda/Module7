786d1b9b8992aa2215d222609be07b4fda4e56cb
Adam Szymajda <adam.szymajda@eu.equinix.com>
2020-10-20 09:50:37 +0200

    Implemented changes

--- wizytowka.py
+++ wizytowka.py
@@ -9,20 +9,20 @@ class BaseContact():
         self.personalemail = email
 
         #Variables
-        self._label_length = len(self.firstname + ' ' + self.lastname)
+        #self._label_length = len(self.firstname + ' ' + self.lastname)
 
     def __str__(self):
-        return f'{self.firstname} {self.lastname} {self.email}'
+        return f'{self.firstname} {self.lastname} {self.personalemail}'
 
     def __repr__(self):
-        return f'firstname={self.firstname} lastname={self.lastname} company={self.company} post={self.post} email={self.email}'
+        return f'firstname={self.firstname} lastname={self.lastname}'
 
     def contact(self):
         print('Wybieram numer {} i dzwonię do {} {}'.format(self.personalphone, self.firstname, self. lastname))
 
     @property
     def label_length(self):
-        return self._label_length
+        return len(self.firstname) + len(self.lastname) +1
 
 
 class BusinessContact(BaseContact):
@@ -38,45 +38,70 @@ class BusinessContact(BaseContact):
 
 # print(issubclass(BusinessContact, BaseContact))
 
+# def generate_fake_names(type, qty):
+#     basenames = []
+#     businessnames = []
+#     fake = Faker()
+#     if type == 'base':
+#         for i in range(qty):
+#             basenames.append(BaseContact(firstname=fake.first_name(),\
+#                 lastname=fake.last_name(),\
+#                 phone=fake.phone_number(),\
+#                 email=fake.email())
+#             )
+#     else:
+#         for i in range(qty):
+#             businessnames.append(BusinessContact(firstname=fake.first_name(),\
+#                 lastname=fake.last_name(), phone=fake.phone_number(),\
+#                 email=fake.email(), company=fake.company(),\
+#                 job=fake.job(), workemail=fake.company_email(),\
+#                 workphone=fake.phone_number())
+#             )
+#     return basenames, businessnames
+
 def generate_fake_names(type, qty):
-    basenames = []
-    businessnames = []
+    # names = []
     fake = Faker()
-    if type == 'base':
-        for i in range(qty):
-            basenames.append(BaseContact(firstname=fake.first_name(),\
-                lastname=fake.last_name(),\
-                phone=fake.phone_number(),\
-                email=fake.email())
-            )
+    if type=='base':
+        names = [BaseContact(firstname=fake.first_name(),\
+            lastname=fake.last_name(),\
+            phone=fake.phone_number(),\
+            email=fake.email())\
+            for i in range(qty) ]
     else:
-        for i in range(qty):
-            businessnames.append(BusinessContact(firstname=fake.first_name(),\
-                lastname=fake.last_name(), phone=fake.phone_number(),\
-                email=fake.email(), company=fake.company(),\
-                job=fake.job(), workemail=fake.company_email(),\
-                workphone=fake.phone_number())
-            )
-    return basenames, businessnames
+        names = [BusinessContact(firstname=fake.first_name(),\
+            lastname=fake.last_name(),\
+            phone=fake.phone_number(),\
+            email=fake.email(),\
+            company=fake.company(),\
+            job=fake.job(),\
+            workemail=fake.company_email(),\
+            workphone=fake.phone_number())
+            for i in range(qty) if type=='business']
+    return names
 
 
-basenamelist, businessnamelist = generate_fake_names('business', 5)
+type = 'business'
 
-print('\nBase')
-print('---------------\n')
+namelist = generate_fake_names(type, 5)
 
-for name in basenamelist:
-    print('{} {} {} {}'.format(name.firstname, name.lastname, name.personalphone, name.personalemail))
-    print(name.label_length)
-    print(name.contact())
+if type == 'base':
 
-print('\nBusiness')
-print('---------------\n')
+    print('\nBase')
+    print('---------------\n')
 
-for name in businessnamelist:
-    print('{} {} {} {} {}'.format(name.firstname, name.lastname, name.personalphone, name.workphone, name.workemail))
-    print(name.label_length)
-    print(name.contact())
+    for name in namelist:
+        print('{} {} {} {}'.format(name.firstname, name.lastname, name.personalphone, name.personalemail))
+        print(name.label_length)
+        name.contact()
+else:
+    print('\nBusiness')
+    print('---------------\n')
+
+    for name in namelist:
+        print('{} {} {} {} {}'.format(name.firstname, name.lastname, name.personalphone, name.workphone, name.workemail))
+        print(name.label_length)
+        name.contact()
 
 
 
